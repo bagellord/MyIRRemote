353fc3748d52037a55a37b701a57d13f466bfa9f
bagellord <bagellord@gmail.com>
2013-09-30 18:06:28 -0500

    Added more buttons
    Added more buttons and the code to make them work. Changed the icon.

--- MyIRRemote/gen/com/drew/myirremote/R.java
+++ MyIRRemote/gen/com/drew/myirremote/R.java
@@ -22,12 +22,27 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int navigation_back=0x7f020001;
+        public static final int navigation_collapse=0x7f020002;
+        public static final int navigation_expand=0x7f020003;
+        public static final int navigation_forward=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
+        public static final int action_settings=0x7f08000e;
         public static final int irRemoteRoot=0x7f080000;
-        public static final int irRemoteTvPower=0x7f080001;
-        public static final int irRemoteXboxPower=0x7f080002;
+        public static final int irRemoteTvPower=0x7f080002;
+        public static final int irRemoteXboxA=0x7f080006;
+        public static final int irRemoteXboxB=0x7f080008;
+        public static final int irRemoteXboxDown=0x7f08000d;
+        public static final int irRemoteXboxLeft=0x7f08000b;
+        public static final int irRemoteXboxPause=0x7f080004;
+        public static final int irRemoteXboxPlay=0x7f080005;
+        public static final int irRemoteXboxPower=0x7f080003;
+        public static final int irRemoteXboxRight=0x7f08000c;
+        public static final int irRemoteXboxUp=0x7f08000a;
+        public static final int irRemoteXboxX=0x7f080007;
+        public static final int irRemoteXboxY=0x7f080009;
+        public static final int linearLayout1=0x7f080001;
     }
     public static final class layout {
         public static final int activity_ir=0x7f030000;
@@ -40,7 +55,17 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
         public static final int ir_remote_tv_power=0x7f050003;
+        public static final int ir_remote_xbox_a=0x7f050007;
+        public static final int ir_remote_xbox_b=0x7f050009;
+        public static final int ir_remote_xbox_down_cd=0x7f05000e;
+        public static final int ir_remote_xbox_left_cd=0x7f05000d;
+        public static final int ir_remote_xbox_pause=0x7f050005;
+        public static final int ir_remote_xbox_play=0x7f050006;
         public static final int ir_remote_xbox_power=0x7f050004;
+        public static final int ir_remote_xbox_right_cd=0x7f05000c;
+        public static final int ir_remote_xbox_up_cd=0x7f05000b;
+        public static final int ir_remote_xbox_x=0x7f050008;
+        public static final int ir_remote_xbox_y=0x7f05000a;
     }
     public static final class style {
         /** 
